fe318387f074743d1e9399616bccbef2acde659f
shubhampatel2610 <shubhu2610@gmail.com>
2025-11-19 22:53:44 +0530

    commit for adding tagline with typewriter package, introline and work button

--- components/commonComponents/MainSection/MainSection.tsx
+++ components/commonComponents/MainSection/MainSection.tsx
@@ -1,13 +1,49 @@
 import { Image } from "primereact/image";
 import "./MainSection.scss";
 import { observer } from "mobx-react-lite";
+import AppConstants from "@/constant/AppConstants";
+import Typewriter from "typewriter-effect";
+import { Button } from "primereact/button";
 
 const MainSection = observer(() => {
   return (
     <div className="mainContainer">
       <div className="parentContainer">
         <div className="profilePictureContainer">
-          <Image src="/siteImages/profile_picture.jpg" alt="Profile Picture" />
+          <Image
+            src={AppConstants.PROFILE_PICTURE_PATH}
+            alt="Profile Picture"
+          />
+        </div>
+        <div className="taglineContainer">
+          {AppConstants.MAIN_SECTION_TAGLINE_1}
+          <span>{AppConstants.MAIN_SECTION_TAGLINE_2}</span>
+        </div>
+        <div className="introlineContainer">
+          {AppConstants.MAIN_SECTION_INTROLINE}
+          <span>
+            <Typewriter
+              options={{
+                strings: [
+                  "Frontend Developer.",
+                  "UI Enthusiast.",
+                  "Web Developer.",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 75,
+                deleteSpeed: 50,
+              }}
+            />
+          </span>
+        </div>
+        <div className="buttonContainer">
+          <Button
+            className="workButton"
+            label={AppConstants.WORK_BUTTON_LABEL}
+            icon="pi pi-arrow-right"
+            iconPos="right"
+          />
         </div>
       </div>
     </div>
--- constant/AppConstants.ts
+++ constant/AppConstants.ts
@@ -12,6 +12,14 @@ class AppConstants {
     { id: 6, url: "#", label: "Testimonials" },
     { id: 7, url: "#", label: "Contact" },
   ];
+
+  // Main Section Constants
+  static PROFILE_PICTURE_PATH = "/siteImages/profile_picture.jpg";
+  static MAIN_SECTION_TAGLINE_1 = "Passionate About Creating Intuitive, Creative, User-Centric ";
+  static MAIN_SECTION_TAGLINE_2 = "Web Solutions.";
+  static MAIN_SECTION_INTROLINE = "Hi, I'm Shubham, a";
+  static WORK_BUTTON_LABEL = "See My Work";
+
 }
 
 export default AppConstants;
