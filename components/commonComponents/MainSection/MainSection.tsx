e0ea358167a2cc5fdfd9f24eb4a14f1c4582ac94
shubhampatel2610 <shubhu2610@gmail.com>
2025-11-19 22:14:21 +0530

    commit for adding profile picture

--- components/commonComponents/MainSection/MainSection.tsx
+++ components/commonComponents/MainSection/MainSection.tsx
@@ -1,10 +1,15 @@
+import { Image } from "primereact/image";
 import "./MainSection.scss";
 import { observer } from "mobx-react-lite";
 
 const MainSection = observer(() => {
   return (
     <div className="mainContainer">
-      <div className="parentContainer">Hero Section</div>
+      <div className="parentContainer">
+        <div className="profilePictureContainer">
+          <Image src="/siteImages/profile_picture.jpg" alt="Profile Picture" />
+        </div>
+      </div>
     </div>
   );
 });
