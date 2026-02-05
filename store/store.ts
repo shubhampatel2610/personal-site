bbcb3fb9c1c1c0f88a0bff19efcfb3b6b550e088
shubhampatel2610 <shubhu2610@gmail.com>
2026-02-05 22:19:24 +0530

    remove unneccesary store

--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,7 @@
-import ExperienceStore from "./ExperienceStore";
 import NavbarStore from "./NavbarStore";
 
 export const rootStore = {
   NavbarStore,
-  ExperienceStore
 };
 
 export type RootStore = typeof rootStore;
