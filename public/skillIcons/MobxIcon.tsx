9c2343780c38e9e2193b181309dfc850c35ed068
shubhampatel2610 <shubhu2610@gmail.com>
2026-02-09 23:55:12 +0530

    commit for fixing and adding new skill icons

--- public/skillIcons/MobxIcon.tsx
+++ public/skillIcons/MobxIcon.tsx
@@ -3,8 +3,6 @@ const MobxIcon = () => {
     <>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="128"
-        height="128"
         viewBox="0 0 128 128"
       >
         <path
